ef0fad6af568d1a409d1cdad4875153b84b9695d
learnor <learnor@gmail.com>
2018-02-27 22:57:59 -0800

    solve p01_int_set

--- algo_project_2/lib/p01_int_set.rb
+++ algo_project_2/lib/p01_int_set.rb
@@ -1,22 +1,31 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num < @store.count && num >= 0
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
@@ -27,18 +36,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num].push(num) unless self.include?(num)
   end
 
   def remove(num)
+    self[num].delete(num) if self.include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,18 +68,25 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count == num_buckets
+    self[num].push(num) unless self.include?(num)
+    @count += 1
   end
 
   def remove(num)
+    self[num].delete(num) if self.include?(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +94,13 @@ class ResizingIntSet
   end
 
   def resize!
+    new_num_buckets = num_buckets * 2
+    new_store = Array.new(new_num_buckets) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |num|
+        new_store[num % new_num_buckets].push(num)
+      end
+    end
+    @store = new_store
   end
 end
