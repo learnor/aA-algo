13ada13a93326cb700e53afbbefc61f556d1254c
learnor <learnor@gmail.com>
2018-02-28 01:52:12 -0800

    solve p03 hash_set

--- algo_project_2/lib/p03_hash_set.rb
+++ algo_project_2/lib/p03_hash_set.rb
@@ -9,18 +9,33 @@ class HashSet
   end
 
   def insert(key)
+    # hashed = key.hash
+    unless self.include?(key)
+      resize! if @count == num_buckets
+      self[key].push(key)
+      @count += 1
+    end
   end
 
   def include?(key)
+    # hashed = key.hash
+    self[key].include?(key)
   end
 
   def remove(key)
+    # hashed = key.hash
+    if self.include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    num = num.hash
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +43,13 @@ class HashSet
   end
 
   def resize!
+    new_num_buckets = num_buckets * 2
+    new_store = Array.new(new_num_buckets) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |num|
+        new_store[num % new_num_buckets].push(num)
+      end
+    end
+    @store = new_store
   end
 end
