e51a1388f7b5c23bfe8add353fc8762d530b3b53
learnor <learnor@gmail.com>
2018-02-28 22:48:09 -0800

    solve heap_sort

--- algo_project_3/lib/heap_sort.rb
+++ algo_project_3/lib/heap_sort.rb
@@ -2,5 +2,14 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    heap = BinaryMinHeap.new
+    until self.empty?
+      heap.push(self.pop)
+    end
+
+    until heap.count == 0
+      self << heap.extract
+    end
+    self
   end
 end
