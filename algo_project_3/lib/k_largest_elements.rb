d6f8f6dabed813bf7906f5104980b1b613abfbeb
learnor <learnor@gmail.com>
2018-02-28 22:58:13 -0800

    solve k_largest_elements

--- algo_project_3/lib/k_largest_elements.rb
+++ algo_project_3/lib/k_largest_elements.rb
@@ -1,5 +1,13 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-
+  heap = BinaryMinHeap.new { |x, y| -1 * (x <=> y) }
+  array.each do |el|
+    heap.push(el)
+  end
+  k_arr = []
+  k.times do
+    k_arr << heap.extract
+  end
+  k_arr
 end
