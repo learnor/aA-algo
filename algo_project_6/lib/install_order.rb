7e51bf8986a73ad2c275aa53b95e13757de1d012
learnor <learnor@gmail.com>
2018-03-06 00:04:09 -0800

    solve install_order

--- algo_project_6/lib/install_order.rb
+++ algo_project_6/lib/install_order.rb
@@ -6,9 +6,15 @@
 # N.B. this is how `npm` works.
 
 # Import any files you need to
-
-
+require 'graph'
+require 'topological_sort'
 
 def install_order(arr)
-
+  max_id = arr.reduce(&:+).max
+  vertices = (1..max_id).to_a.map { |e| Vertex.new(e) }
+  arr.each do |tuple|
+    Edge.new(vertices[tuple[1] - 1], vertices[tuple[0] - 1])
+  end
+  # p topological_sort(vertices.values).map(&:value)
+  topological_sort(vertices).map(&:value)
 end
